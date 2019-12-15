2653f9fae928308f3972b415d89416d014a5344e
Harrybenji <Harrybenji2000@gmail.com>
2019-12-15 01:12:51 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import "package:flutter/material.dart";
+
+
+void main (){
+
+  runApp(
+      Center(
+        child:Text (
+            "Hello world",
+            textDirection: TextDirection.ltr
+        ),
+
+      )
+
+  );
+
+
+
+
+
+
+} 
